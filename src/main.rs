22e16867bbba60c2bd300e862d87e272fc53516e
Jakob <31919330+Jakobzs@users.noreply.github.com>
2023-11-01 17:54:23 +0100

    add sample for sysinfo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use sysinfo::{ProcessExt, System, SystemExt};
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys = System::new_all();
+
+    // First we update all information of our `System` struct.
+    sys.refresh_all();
+
+    // We display all disks' information:
+    println!("=> disks:");
+    for disk in sys.disks() {
+        println!("{:?}", disk);
+    }
+
+    println!("total memory: {} bytes", sys.total_memory());
+    println!("used memory : {} bytes", sys.used_memory());
+    println!("total swap  : {} bytes", sys.total_swap());
+    println!("used swap   : {} bytes", sys.used_swap());
+
+    // Sleep for 5 seconds.
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+    // Sys refresh only processes' information.
+    sys.refresh_processes();
+
+    for (pid, process) in sys.processes() {
+        if process.cpu_usage() > 0.0 {
+            println!(
+                "Pid: [{}], process name: {}, cpu usage: {}, disk usage: {:?}",
+                pid,
+                process.name(),
+                process.cpu_usage(),
+                process.disk_usage()
+            );
+        }
+    }
 }
