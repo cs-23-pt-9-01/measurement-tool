a3c140721eef934457d609789d8dc1b1b649a3a3
Jakob <31919330+Jakobzs@users.noreply.github.com>
2023-11-06 13:15:28 +0100

    Log only what changes, github workflow

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,23 @@ use sysinfo::{CpuExt, DiskUsage, PidExt, ProcessExt, System};
 use systemctl::UnitList;
 use time::{format_description::well_known::Rfc3339, OffsetDateTime};
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, PartialEq)]
 struct MeasurementData {
     timestamp: String,
     used_memory: u64,
     used_swap: u64,
-    process_data: Vec<ProcessData>,
-    cpu_data: Vec<CpuData>,
-    units: Vec<UnitList>,
+    process_data: Option<Vec<ProcessData>>,
+    cpu_data: Option<Vec<CpuData>>,
+    units: Option<Vec<UnitList>>,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, PartialEq)]
 struct CpuData {
     cpu_usage: f32,
     frequency: u64,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, PartialEq, Clone)]
 struct ProcessData {
     pid: u32,
     name: String,
@@ -40,70 +40,111 @@ fn main() {
         .open("idle-log.txt")
         .unwrap();
 
+    let mut bak_data = MeasurementData {
+        timestamp: OffsetDateTime::now_utc().format(&Rfc3339).unwrap(),
+        used_memory: 0,
+        used_swap: 0,
+        process_data: None,
+        cpu_data: None,
+        units: None,
+    };
+
     loop {
-        // Refresh processes
-        sys.refresh_processes();
-        let mut process_data: Vec<ProcessData> = Vec::new();
-        for (pid, process) in sys.processes() {
-            if process.cpu_usage() > 0.0 {
-                process_data.push(ProcessData {
-                    pid: pid.as_u32(),
-                    name: process.name().to_string(),
-                    cpu_usage: process.cpu_usage(),
-                    memory_usage: process.memory(),
-                    disk_usage: process.disk_usage(),
-                });
-            }
-        }
-
-        // Get CPU data
-        let mut cpu_data = Vec::new();
-        sys.refresh_cpu();
-        for cpu in sys.cpus() {
-            cpu_data.push(CpuData {
-                cpu_usage: cpu.cpu_usage(),
-                frequency: cpu.frequency(),
-            });
-        }
-
-        #[cfg(target_os = "linux")]
-        let output_data = {
-            //let enabled_services = systemctl::list_enabled_services().unwrap();
-            let units = systemctl::list_units_full(None, None, None).unwrap();
-
-            MeasurementData {
-                timestamp: OffsetDateTime::now_utc().format(&Rfc3339).unwrap(),
-                used_memory: sys.used_memory(),
-                used_swap: sys.used_swap(),
-                process_data,
-                cpu_data,
-                units,
-            }
-        };
-
-        #[cfg(target_os = "windows")]
-        let output_data = MeasurementData {
-            timestamp: OffsetDateTime::now_utc().format(&Rfc3339).unwrap(),
+        let curr_time = OffsetDateTime::now_utc().format(&Rfc3339).unwrap();
+        let mut output_data = MeasurementData {
+            timestamp: bak_data.timestamp.clone(),
             used_memory: sys.used_memory(),
             used_swap: sys.used_swap(),
-            process_data,
-            cpu_data,
-            units: Vec::new(),
+            process_data: None,
+            cpu_data: None,
+            units: None,
         };
 
-        //let mut testy = serde_json::to_string(&sys).unwrap();
-        let mut output_data_string = serde_json::to_string(&output_data).unwrap();
-        output_data_string.push('\n');
+        // Get processes
+        get_processes(&mut sys, &bak_data, &mut output_data);
 
-        file.write_all(output_data_string.as_bytes()).unwrap();
+        // Get CPU data
+        get_cpu_data(&mut sys, &bak_data, &mut output_data);
 
-        // TODO: Fix CPU usage being 0.0, not working yet
-        /*
-        use std::thread;
-        thread::sleep(System::MINIMUM_CPU_UPDATE_INTERVAL);
-        */
+        // Get systemd units
+        #[cfg(target_os = "linux")]
+        get_systemd_units(&bak_data, &mut output_data);
+
+        // Write to file
+        if output_data != bak_data {
+            // Replace timestamp with current time
+            output_data.timestamp = curr_time;
+
+            let mut output_data_string = serde_json::to_string(&output_data).unwrap();
+            output_data_string.push('\n');
+
+            file.write_all(output_data_string.as_bytes()).unwrap();
+
+            bak_data = output_data;
+        }
     }
 }
+
+#[cfg(target_os = "linux")]
+fn get_systemd_units(bak_data: &MeasurementData, output_data: &mut MeasurementData) {
+    let units = systemctl::list_units_full(None, None, None).unwrap();
+    if let Some(bak_units_ref) = &bak_data.units {
+        if bak_units_ref != &units {
+            output_data.units = Some(units);
+        }
+    } else {
+        output_data.units = Some(units);
+    }
+}
+
+fn get_cpu_data(sys: &mut System, bak_data: &MeasurementData, output_data: &mut MeasurementData) {
+    let mut cpu_data = Vec::new();
+    sys.refresh_cpu();
+    for cpu in sys.cpus() {
+        cpu_data.push(CpuData {
+            cpu_usage: cpu.cpu_usage(),
+            frequency: cpu.frequency(),
+        });
+    }
+
+    if let Some(bak_cpu_data_ref) = &bak_data.cpu_data {
+        if bak_cpu_data_ref != &cpu_data {
+            output_data.cpu_data = Some(cpu_data);
+        }
+    } else {
+        output_data.cpu_data = Some(cpu_data);
+    }
+}
+
+fn get_processes(sys: &mut System, bak_data: &MeasurementData, output_data: &mut MeasurementData) {
+    sys.refresh_processes();
+    let mut process_data: Vec<ProcessData> = Vec::new();
+    for (pid, process) in sys.processes() {
+        if process.cpu_usage() > 0.0 {
+            process_data.push(ProcessData {
+                pid: pid.as_u32(),
+                name: process.name().to_string(),
+                cpu_usage: process.cpu_usage(),
+                memory_usage: process.memory(),
+                disk_usage: process.disk_usage(),
+            });
+        }
+    }
+
+    if let Some(bak_process_data_ref) = &bak_data.process_data {
+        if bak_process_data_ref != &process_data {
+            output_data.process_data = Some(process_data);
+        }
+    } else {
+        output_data.process_data = Some(process_data);
+    }
+}
+
+// TODO: Fix CPU usage being 0.0, not working yet
+/*
+use std::thread;
+thread::sleep(System::MINIMUM_CPU_UPDATE_INTERVAL);
+*/
 
 /*
 loop {
